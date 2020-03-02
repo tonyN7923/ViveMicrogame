18021ae31f6ff11d3dc78513a97b525b27f79e94
Brandon Head <bhead43@gmail.com>
2020-03-02 11:51:27 -0600

    sound working

---
+++ Vive Microgame/Assets/bowling.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bowling : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioSource pin;
+    public AudioClip pinKnock;
+    public AudioClip strike;
+    public AudioClip turkey;
+
+    public bool didHit = false;
+    public bool didStrike = false;
+    public bool didTurkey = false;
+
+    void Start()
+    {
+        pin = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (didHit)
+        {
+            pin.PlayOneShot(pinKnock);
+            didHit = false;
+        }
+
+        if (didStrike)
+        {
+            pin.PlayOneShot(strike);
+            didStrike = false;
+        }
+
+        if (didTurkey)
+        {
+            pin.PlayOneShot(turkey);
+            didTurkey = false;
+        }
+    }
+}
